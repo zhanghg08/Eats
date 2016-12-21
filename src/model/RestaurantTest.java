aa5f0fd65bd2fab5bc697f93d0ec18a8356bfdba
Raymond Zou <raymondjoe16@gmail.com>
2016-12-20 23:20:28 -0500

    add stringtoJasonArray Test

--- src/model/RestaurantTest.java
+++ src/model/RestaurantTest.java
@@ -2,6 +2,9 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,6 +16,36 @@ import org.junit.rules.TestName;
 
 public class RestaurantTest {
 	Restaurant restaurant;
+	boolean jsonArrayStringEquals(JSONArray arr1, JSONArray arr2) {
+		Set<String> set1 = new HashSet<String>();
+		Set<String> set2 = new HashSet<String>();
+		try {
+			for (int i = 0; i < arr1.length(); i++) {
+				set1.add(arr1.getString(i));
+			}
+			for (int i = 0; i < arr2.length(); i++) {
+				set2.add(arr2.getString(i));
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return set1.equals(set2);
+	}
+	@Test
+	public void testStringToJSONArray() {
+		JSONArray jsonArray = new JSONArray();
+		assertTrue(jsonArrayStringEquals(jsonArray, Restaurant.stringToJSONArray(",")));
+		jsonArray.put("");
+		assertTrue(jsonArrayStringEquals(jsonArray, Restaurant.stringToJSONArray("")));
+		jsonArray.put("Chinese");
+		assertTrue(jsonArrayStringEquals(jsonArray, Restaurant.stringToJSONArray(",Chinese")));
+		jsonArray.put("");
+		assertTrue(jsonArrayStringEquals(jsonArray, Restaurant.stringToJSONArray(",Chinese,")));
+		jsonArray.put("Italian");
+		assertTrue(jsonArrayStringEquals(jsonArray, Restaurant.stringToJSONArray("Italian,,Chinese,")));
+	}
 	@Test
 	public void testJsonArrayToString() {
 		JSONArray jsonArray = new JSONArray();
